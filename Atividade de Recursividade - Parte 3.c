eb28adaa0c713d74eaebf54395ea25e05bf1ba7b
Victor Moreira <victorvicmr@gmail.com>
2025-04-28 20:27:13 -0300

    Busca Binária

--- Atividade de Recursividade - Parte 3.c
+++ Atividade de Recursividade - Parte 3.c
@@ -186,59 +186,95 @@
 
 // 24) Quick Sort {Com ajuda do Chat GPT}
 
+// #include <stdio.h>
+
+// void trocar(int *a, int *b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+// int particionar(int array[], int inicio, int fim) {
+
+//     int pivo = array[fim];  
+//     int i = inicio - 1; 
+
+//     for (int j = inicio; j < fim; j++) {
+//         if (array[j] <= pivo) {
+//             i++;
+//             trocar(&array[i], &array[j]);
+//         }
+//     }
+
+//     trocar(&array[i + 1], &array[fim]);
+//     return i + 1;
+// }
+
+// void quickSort(int array[], int inicio, int fim) {
+//     if (inicio < fim) {
+//         int pi = particionar(array, inicio, fim);
+
+//         quickSort(array, inicio, pi - 1);
+//         quickSort(array, pi + 1, fim);
+//     }
+// }
+
+// void imprimirArray(int array[], int tamanho) {
+//     printf("{");
+//     for (int i = 0; i < tamanho; i++) {
+//         printf("%d", array[i]);
+//         if (i < tamanho - 1) {
+//             printf(", ");
+//         }
+//     }
+//     printf("}\n");
+// }
+
+// int main() {
+//     int array[] = {9, 2, 5, 1, 7};
+//     int tamanho = sizeof(array) / sizeof(array[0]);
+
+//     printf("Array original: ");
+//     imprimirArray(array, tamanho);
+
+//     quickSort(array, 0, tamanho - 1);
+
+//     printf("Array ordenado: ");
+//     imprimirArray(array, tamanho);
+// }
+
+// 25) Busca Binária {Com ajuda do Chat GPT}
+
 #include <stdio.h>
 
-void trocar(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int particionar(int array[], int inicio, int fim) {
-
-    int pivo = array[fim];  
-    int i = inicio - 1; 
-
-    for (int j = inicio; j < fim; j++) {
-        if (array[j] <= pivo) {
-            i++;
-            trocar(&array[i], &array[j]);
-        }
+int buscaBinaria(int array[], int inicio, int fim, int valor) {
+    if (inicio > fim) {
+        return -1;
     }
 
-    trocar(&array[i + 1], &array[fim]);
-    return i + 1;
-}
+    int meio = (inicio + fim) / 2;
 
-void quickSort(int array[], int inicio, int fim) {
-    if (inicio < fim) {
-        int pi = particionar(array, inicio, fim);
-
-        quickSort(array, inicio, pi - 1);
-        quickSort(array, pi + 1, fim);
+    if (array[meio] == valor) {
+        return meio;
     }
-}
 
-void imprimirArray(int array[], int tamanho) {
-    printf("{");
-    for (int i = 0; i < tamanho; i++) {
-        printf("%d", array[i]);
-        if (i < tamanho - 1) {
-            printf(", ");
-        }
+    if (valor < array[meio]) {
+        return buscaBinaria(array, inicio, meio - 1, valor);
+    } else {
+        return buscaBinaria(array, meio + 1, fim, valor);
     }
-    printf("}\n");
 }
 
 int main() {
-    int array[] = {9, 2, 5, 1, 7};
+    int array[] = {1, 3, 5, 7, 9};
     int tamanho = sizeof(array) / sizeof(array[0]);
+    int valor = 9;
 
-    printf("Array original: ");
-    imprimirArray(array, tamanho);
+    int indice = buscaBinaria(array, 0, tamanho - 1, valor);
 
-    quickSort(array, 0, tamanho - 1);
-
-    printf("Array ordenado: ");
-    imprimirArray(array, tamanho);
+    if (indice != -1) {
+        printf("Elemento %d encontrado no indice %d.\n", valor, indice);
+    } else {
+        printf("Elemento %d não encontrado.\n", valor);
+    }
 }
