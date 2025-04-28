96cb06948f4b2d399f50167410da9681cbc1110c
Victor Moreira <victorvicmr@gmail.com>
2025-04-28 19:57:51 -0300

    Subconjuntos de um Array

--- Atividade de Recursividade - Parte 3.c
+++ Atividade de Recursividade - Parte 3.c
@@ -1,19 +1,61 @@
 // 21) Combinações de um Array
 
+// #include <stdio.h>
+// #include <string.h>
+
+// void combinacoesArray (int arrayOriginal[], int tamanho, int K, int indice, int arrayAuxiliar[], int posicao) {
+
+//     if (K <= 0) {
+
+//         printf ("{");
+
+//         for (int j = 0; j <= posicao - 1; j++) {
+
+//             printf ("%d", arrayAuxiliar[j]);
+
+//             if (j < posicao - 1) {
+
+//                 printf (", ");
+//             }
+//         }
+
+//         printf ("}\n");
+//         return;
+//     }
+
+
+//     for (int i = indice; i < tamanho; i++) {
+
+//         arrayAuxiliar[posicao] = arrayOriginal[i];
+//         combinacoesArray (arrayOriginal, tamanho, K - 1, i + 1, arrayAuxiliar, posicao + 1);
+//     }
+// }
+
+// int main () {
+
+//     int array[] = {1, 2, 3, 4, 5, 6};
+//     int tamanho = sizeof (array) / sizeof (array[0]);
+//     int K = 2;
+//     int arrayAuxiliar[K];
+
+//     combinacoesArray (array, tamanho, K, 0, arrayAuxiliar, 0);
+// }
+
+// 22) Subconjuntos de um Array
+
 #include <stdio.h>
-#include <string.h>
 
-void combinacoesArray (int arrayOriginal[], int tamanho, int K, int indice, int arrayAuxiliar[], int posicao) {
+void subconjuntosArray (int array[], int tamanho, int arrayAuxiliar[], int indice, int posicao) {
 
-    if (K <= 0) {
+    if (indice == tamanho) {
 
         printf ("{");
 
-        for (int j = 0; j <= posicao - 1; j++) {
+        for (int i = 0; i < posicao; i++) {
 
-            printf ("%d", arrayAuxiliar[j]);
+            printf ("%d", arrayAuxiliar[i]);
 
-            if (j < posicao - 1) {
+            if (i < posicao - 1) {
 
                 printf (", ");
             }
@@ -23,20 +65,21 @@ void combinacoesArray (int arrayOriginal[], int tamanho, int K, int indice, int 
         return;
     }
 
+    arrayAuxiliar[posicao] = array[indice];
 
-    for (int i = indice; i < tamanho; i++) {
+    subconjuntosArray (array, tamanho, arrayAuxiliar, indice + 1, posicao + 1);
 
-        arrayAuxiliar[posicao] = arrayOriginal[i];
-        combinacoesArray (arrayOriginal, tamanho, K - 1, i + 1, arrayAuxiliar, posicao + 1);
-    }
+    subconjuntosArray (array, tamanho, arrayAuxiliar, indice + 1,  posicao);
 }
 
 int main () {
 
-    int array[] = {1, 2, 3, 4, 5, 6};
-    int tamanho = (sizeof (array) / sizeof (array[0]));
-    int K = 2;
-    int arrayAuxiliar[K];
+    int array[] = {1, 2, 3, 4, 5};
 
-    combinacoesArray (array, tamanho, K, 0, arrayAuxiliar, 0);
+    int tamanho = sizeof (array) / sizeof (array[0]);
+    int arrayAuxiliar[tamanho];
+
+    printf ("Subconjuntos do array:\n");
+
+    subconjuntosArray (array, tamanho, arrayAuxiliar, 0, 0);
 }
